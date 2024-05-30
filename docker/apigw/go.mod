a8baea6ed9ae1c85bdcd9a3f08b63ed8b229a31f
fcracker79 <fcracker79@gmail.com>
2024-05-30 08:49:00 +0200

    Added traceId and spanId to apigw

--- docker/apigw/go.mod
+++ docker/apigw/go.mod
@@ -3,11 +3,30 @@ module github.com/fcracker79/k8s-experiment/docker/rest/apigw
 go 1.22.3
 
 require (
-	github.com/go-chi/chi/v5 v5.0.12 // indirect
+	contrib.go.opencensus.io/exporter/ocagent v0.6.0
+	github.com/go-chi/chi/v5 v5.0.12
+	google.golang.org/grpc v1.64.0
+	google.golang.org/protobuf v1.34.1
+)
+
+require (
+	github.com/census-instrumentation/opencensus-proto v0.4.1 // indirect
+	github.com/golang/groupcache v0.0.0-20200121045136-8c9f03a8e57e // indirect
+	github.com/golang/protobuf v1.5.4 // indirect
+	github.com/grpc-ecosystem/grpc-gateway/v2 v2.11.3 // indirect
+	github.com/hashicorp/golang-lru v0.5.1 // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.19 // indirect
+	github.com/rs/zerolog v1.33.0 // indirect
+	go.opencensus.io v0.24.0 // indirect
+	go.opentelemetry.io/otel v1.27.0 // indirect
+	go.opentelemetry.io/otel/trace v1.27.0 // indirect
 	golang.org/x/net v0.22.0 // indirect
+	golang.org/x/sync v0.6.0 // indirect
 	golang.org/x/sys v0.18.0 // indirect
 	golang.org/x/text v0.14.0 // indirect
+	google.golang.org/api v0.7.0 // indirect
+	google.golang.org/genproto v0.0.0-20230711160842-782d3b101e98 // indirect
+	google.golang.org/genproto/googleapis/api v0.0.0-20240318140521-94a12d6c2237 // indirect
 	google.golang.org/genproto/googleapis/rpc v0.0.0-20240318140521-94a12d6c2237 // indirect
-	google.golang.org/grpc v1.64.0 // indirect
-	google.golang.org/protobuf v1.34.1 // indirect
 )
--- docker/apigw/pkg/main.go
+++ docker/apigw/pkg/main.go
@@ -1,214 +1,259 @@
 package main
 
 import (
-    "context"
-    "encoding/json"
-    "fmt"
-    "io/ioutil"
-    "log"
-    "net/http"
-    "os"
-    "strings"
-    "time"
+	"context"
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"strings"
+	"time"
 
-    pb "github.com/fcracker79/k8s-experiment/docker/rest/apigw/proto/user"
+	pb "github.com/fcracker79/k8s-experiment/docker/rest/apigw/proto/user"
 
-    "github.com/go-chi/chi/v5"
-    "google.golang.org/grpc"
+	"contrib.go.opencensus.io/exporter/ocagent"
+	"github.com/go-chi/chi/v5"
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
+	"go.opencensus.io/plugin/ochttp"
+	"go.opencensus.io/plugin/ochttp/propagation/b3"
+	"go.opencensus.io/trace"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
 )
 
 type Company struct {
-    ID          string    `json:"id"`
-    Name        string    `json:"name"`
-    Description string    `json:"description"`
-    CreatedAt   time.Time `json:"created_at"`
-    UpdatedAt   time.Time `json:"updated_at"`
+	ID          string    `json:"id"`
+	Name        string    `json:"name"`
+	Description string    `json:"description"`
+	CreatedAt   time.Time `json:"created_at"`
+	UpdatedAt   time.Time `json:"updated_at"`
+}
+
+func LogMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		span := trace.FromContext(r.Context())
+		traceID := span.SpanContext().TraceID.String()
+		spanID := span.SpanContext().SpanID.String()
+		log := zerolog.New(os.Stderr).With().Timestamp().
+			Str("traceId", traceID).
+			Str("spanId", spanID).
+			Logger()
+		ctx := log.WithContext(r.Context())
+		log.Info().Msg("Request received")
+		next.ServeHTTP(w, r.WithContext(ctx))
+	})
 }
 
 func main() {
-    r := chi.NewRouter()
+	r := chi.NewRouter()
+	ocagentHost := os.Getenv("OC_AGENT_HOST")
+	oce, err := ocagent.NewExporter(
+		ocagent.WithInsecure(),
+		ocagent.WithReconnectionPeriod(5*time.Second),
+		ocagent.WithAddress(ocagentHost),
+		ocagent.WithServiceName("apigw"))
+	if err != nil {
+		log.Fatal().Err(err).Msg("Failed to create ocagent-exporter")
+	}
+	trace.RegisterExporter(oce)
 
-    // User endpoints
-    r.Get("/users/{id}", getUser)
-    r.Post("/users", createUser)
-    r.Delete("/users/{id}", deleteUser)
+	r.Use(func(next http.Handler) http.Handler {
+		return &ochttp.Handler{
+			Handler:     next,
+			Propagation: &b3.HTTPFormat{},
+		}
+	})
+	r.Use(LogMiddleware)
 
-    // Company endpoints
-    r.Get("/companies/{id}", getCompany)
-    r.Get("/companies", getAllCompanies)
-    r.Post("/companies", createCompany)
-    r.Delete("/companies/{id}", deleteCompany)
+	// User endpoints
+	r.Get("/users/{id}", getUser)
+	r.Post("/users", createUser)
+	r.Delete("/users/{id}", deleteUser)
 
-    tcpPort := getEnvString("TCP_PORT")
-    fmt.Printf("Listening port %s\n", tcpPort)
-    http.ListenAndServe(fmt.Sprintf(":%s", tcpPort), r)
+	// Company endpoints
+	r.Get("/companies/{id}", getCompany)
+	r.Get("/companies", getAllCompanies)
+	r.Post("/companies", createCompany)
+	r.Delete("/companies/{id}", deleteCompany)
+
+	tcpPort := getEnvString("TCP_PORT")
+	fmt.Printf("Listening port %s\n", tcpPort)
+	http.ListenAndServe(fmt.Sprintf(":%s", tcpPort), r)
 }
 
 func getUserGrpcEndpoint() string {
-    return fmt.Sprintf("%s:%s", getEnvString("GRPC_USER_HOST"), getEnvString("GRPC_USER_PORT"))
+	return fmt.Sprintf("%s:%s", getEnvString("GRPC_USER_HOST"), getEnvString("GRPC_USER_PORT"))
 }
 
 func getCompanyHttpEndpoint() string {
-    return fmt.Sprintf("http://%s:%s", getEnvString("REST_COMPANY_HOST"), getEnvString("REST_COMPANY_PORT"))
+	return fmt.Sprintf("http://%s:%s", getEnvString("REST_COMPANY_HOST"), getEnvString("REST_COMPANY_PORT"))
 }
 
 func getEnvString(env string) string {
-    if envVar, exists := os.LookupEnv(env); exists {
-        return envVar
-    } else {
-        panic(fmt.Sprintf("%s not set", env))
-    }
+	if envVar, exists := os.LookupEnv(env); exists {
+		return envVar
+	} else {
+		panic(fmt.Sprintf("%s not set", env))
+	}
 }
 
 func getUser(w http.ResponseWriter, r *http.Request) {
-    id := chi.URLParam(r, "id")
-    connection, err := grpc.Dial(getUserGrpcEndpoint(), grpc.WithInsecure())
-    if err != nil {
-        log.Fatalf("did not connect: %v", err)
-    }
-    defer connection.Close()
-    c := pb.NewUserServiceClient(connection)
-    ctx, cancel := context.WithTimeout(context.Background(), time.Second)
-    defer cancel()
-    user, err := c.GetUser(ctx, &pb.User{Id: id})
-    if err != nil {
-        log.Fatalf("could not fetch user: %v", err)
-    }
-    data, _ := json.Marshal(user)
-    w.Header().Set("Content-Type", "application/json")
-    w.Write(data)
+	id := chi.URLParam(r, "id")
+	connection, err := createGrpcConnection()
+	if err != nil {
+		zerolog.Ctx(r.Context()).Fatal().Err(err).Msg("did not connect")
+	}
+	defer connection.Close()
+	c := pb.NewUserServiceClient(connection)
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+	user, err := c.GetUser(ctx, &pb.User{Id: id})
+	if err != nil {
+		log.Fatal().Err(err).Msg("could not fetch user")
+	}
+	data, _ := json.Marshal(user)
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(data)
 }
 
 func createUser(w http.ResponseWriter, r *http.Request) {
-    body, err := ioutil.ReadAll(r.Body)
-    if err != nil {
-        log.Fatalf("could not read request body: %v", err)
-    }
-    var user pb.User
-    err = json.Unmarshal(body, &user)
-    if err != nil {
-        log.Fatalf("could not unmarshal request body: %v", err)
-    }
-    connection, err := grpc.Dial(getUserGrpcEndpoint(), grpc.WithInsecure())
-    if err != nil {
-        log.Fatalf("did not connect: %v", err)
-    }
-    defer connection.Close()
-    c := pb.NewUserServiceClient(connection)
-    ctx, cancel := context.WithTimeout(context.Background(), time.Second)
-    defer cancel()
-    createdUser, err := c.CreateUser(ctx, &user)
-    if err != nil {
-        log.Fatalf("could not create user: %v", err)
-    }
-    data, _ := json.Marshal(createdUser)
-    w.Header().Set("Content-Type", "application/json")
-    w.Write(data)
+	body, err := io.ReadAll(r.Body)
+	if err != nil {
+		log.Fatal().Err(err).Msg("could not read request body")
+	}
+	var user pb.User
+	err = json.Unmarshal(body, &user)
+	if err != nil {
+		zerolog.Ctx(r.Context()).Fatal().Err(err).Msg("could not unmarshal request body")
+	}
+	connection, err := createGrpcConnection()
+	if err != nil {
+		zerolog.Ctx(r.Context()).Fatal().Err(err).Msg("did not connect")
+	}
+	defer connection.Close()
+	c := pb.NewUserServiceClient(connection)
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+	createdUser, err := c.CreateUser(ctx, &user)
+	if err != nil {
+		zerolog.Ctx(r.Context()).Fatal().Err(err).Msg("could not create user")
+	}
+	data, _ := json.Marshal(createdUser)
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(data)
 }
 
 func deleteUser(w http.ResponseWriter, r *http.Request) {
-    id := chi.URLParam(r, "id")
-    connection, err := grpc.Dial(getUserGrpcEndpoint(), grpc.WithInsecure())
-    if err != nil {
-        log.Fatalf("did not connect: %v", err)
-    }
-    defer connection.Close()
-    c := pb.NewUserServiceClient(connection)
-    ctx, cancel := context.WithTimeout(context.Background(), time.Second)
-    defer cancel()
-    _, err = c.DeleteUser(ctx, &pb.User{Id: id})
-    if err != nil {
-        log.Fatalf("could not delete user: %v", err)
-    }
-    w.Write([]byte("User deleted"))
+	id := chi.URLParam(r, "id")
+	connection, err := createGrpcConnection()
+	if err != nil {
+		zerolog.Ctx(r.Context()).Fatal().Err(err).Msg("did not connect")
+	}
+	defer connection.Close()
+	c := pb.NewUserServiceClient(connection)
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+	_, err = c.DeleteUser(ctx, &pb.User{Id: id})
+	if err != nil {
+		zerolog.Ctx(r.Context()).Fatal().Err(err).Msg("could not delete user")
+	}
+	w.Write([]byte("User deleted"))
+}
+
+func createGrpcConnection() (*grpc.ClientConn, error) {
+	connection, err := grpc.NewClient(
+		getUserGrpcEndpoint(), grpc.WithTransportCredentials(insecure.NewCredentials()))
+	return connection, err
 }
 
 func getCompany(w http.ResponseWriter, r *http.Request) {
-    id := chi.URLParam(r, "id")
-    resp, err := http.Get(fmt.Sprintf("%s/companies/%s", getCompanyHttpEndpoint(), id))
-    if err != nil {
-        log.Fatalf("could not fetch company: %v", err)
-    }
-    defer resp.Body.Close()
-    if resp.StatusCode == http.StatusOK {
-        bodyBytes, err := ioutil.ReadAll(resp.Body)
-        if err != nil {
-            log.Fatal(err)
-        }
-        bodyString := string(bodyBytes)
-        w.Write([]byte(bodyString))
-    } else {
-        w.Write([]byte("Company not found"))
-    }
+	id := chi.URLParam(r, "id")
+	resp, err := http.Get(fmt.Sprintf("%s/companies/%s", getCompanyHttpEndpoint(), id))
+	if err != nil {
+		zerolog.Ctx(r.Context()).Fatal().Err(err).Msg("could not fetch company")
+	}
+	defer resp.Body.Close()
+	if resp.StatusCode == http.StatusOK {
+		bodyBytes, err := io.ReadAll(resp.Body)
+		if err != nil {
+			zerolog.Ctx(r.Context()).Fatal().Err(err).Msg("could not read resp body")
+		}
+		bodyString := string(bodyBytes)
+		w.Write([]byte(bodyString))
+	} else {
+		w.Write([]byte("Company not found"))
+	}
 }
 
 func getAllCompanies(w http.ResponseWriter, r *http.Request) {
-    resp, err := http.Get(fmt.Sprintf("%s/companies", getCompanyHttpEndpoint()))
-    if err != nil {
-        log.Fatalf("could not fetch companies: %v", err)
-    }
-    defer resp.Body.Close()
-    if resp.StatusCode == http.StatusOK {
-        bodyBytes, err := ioutil.ReadAll(resp.Body)
-        if err != nil {
-            log.Fatal(err)
-        }
-        bodyString := string(bodyBytes)
-        w.Write([]byte(bodyString))
-    } else {
-        w.Write([]byte("Could not fetch companies"))
-    }
+	resp, err := http.Get(fmt.Sprintf("%s/companies", getCompanyHttpEndpoint()))
+	if err != nil {
+		zerolog.Ctx(r.Context()).Fatal().Err(err).Msg("could not fetch companies")
+	}
+	defer resp.Body.Close()
+	if resp.StatusCode == http.StatusOK {
+		bodyBytes, err := io.ReadAll(resp.Body)
+		if err != nil {
+			zerolog.Ctx(r.Context()).Fatal().Err(err).Msg("could not read resp body")
+		}
+		bodyString := string(bodyBytes)
+		w.Write([]byte(bodyString))
+	} else {
+		w.Write([]byte("Could not fetch companies"))
+	}
 }
 
 func createCompany(w http.ResponseWriter, r *http.Request) {
-    body, err := ioutil.ReadAll(r.Body)
-    if err != nil {
-        log.Fatalf("could not read request body: %v", err)
-    }
+	body, err := io.ReadAll(r.Body)
+	if err != nil {
+		zerolog.Ctx(r.Context()).Fatal().Err(err).Msg("could not read request body")
+	}
 
-    var company Company
-    err = json.Unmarshal(body, &company)
-    if err != nil {
-        log.Fatalf("could not unmarshal request body: %v", err)
-    }
+	var company Company
+	err = json.Unmarshal(body, &company)
+	if err != nil {
+		zerolog.Ctx(r.Context()).Fatal().Err(err).Msg("could not unmarshal request body")
+	}
 
-    jsonStr := string(body)
+	jsonStr := string(body)
 
-    resp, err := http.Post(fmt.Sprintf("%s/companies", getCompanyHttpEndpoint()), "application/json", strings.NewReader(jsonStr))
-    if err != nil {
-        log.Fatalf("could not create company: %v", err)
-    }
-    defer resp.Body.Close()
+	resp, err := http.Post(fmt.Sprintf("%s/companies", getCompanyHttpEndpoint()), "application/json", strings.NewReader(jsonStr))
+	if err != nil {
+		zerolog.Ctx(r.Context()).Fatal().Err(err).Msg("could not create company")
+	}
+	defer resp.Body.Close()
 
-    if resp.StatusCode == http.StatusOK {
-        bodyBytes, err := ioutil.ReadAll(resp.Body)
-        if err != nil {
-            log.Fatal(err)
-        }
-        bodyString := string(bodyBytes)
-        w.Write([]byte(bodyString))
-    } else {
-        w.Write([]byte("Could not create company"))
-    }
+	if resp.StatusCode == http.StatusOK {
+		bodyBytes, err := io.ReadAll(resp.Body)
+		if err != nil {
+			log.Fatal().Err(err).Msg("could not read resp body")
+		}
+		bodyString := string(bodyBytes)
+		w.Write([]byte(bodyString))
+	} else {
+		w.Write([]byte("Could not create company"))
+	}
 }
 
 func deleteCompany(w http.ResponseWriter, r *http.Request) {
-    id := chi.URLParam(r, "id")
+	id := chi.URLParam(r, "id")
 
-    req, err := http.NewRequest("DELETE", fmt.Sprintf("%s/companies/%s", getCompanyHttpEndpoint(), id), nil)
-    if err != nil {
-        log.Fatalf("could not create delete request: %v", err)
-    }
+	req, err := http.NewRequest("DELETE", fmt.Sprintf("%s/companies/%s", getCompanyHttpEndpoint(), id), nil)
+	if err != nil {
+		zerolog.Ctx(r.Context()).Fatal().Err(err).Msg("could not create delete request")
+	}
 
-    resp, err := http.DefaultClient.Do(req)
-    if err != nil {
-        log.Fatalf("could not delete company: %v", err)
-    }
-    defer resp.Body.Close()
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		zerolog.Ctx(r.Context()).Fatal().Err(err).Msg("could not delete company")
+	}
+	defer resp.Body.Close()
 
-    if resp.StatusCode == http.StatusOK {
-        w.Write([]byte("Company deleted"))
-    } else {
-        w.Write([]byte("Could not delete company"))
-    }
+	if resp.StatusCode == http.StatusOK {
+		w.Write([]byte("Company deleted"))
+	} else {
+		w.Write([]byte("Could not delete company"))
+	}
 }
